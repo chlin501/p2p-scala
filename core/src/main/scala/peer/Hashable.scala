d730c3688836dce06c460ed6c7d596e546759f5c
Chia-Hung Lin <clin4j@googlemail.com>
2021-06-19 22:37:32 +0800

    Switch to Hashable for Id hashing

--- core/src/main/scala/peer/Hashable.scala
+++ core/src/main/scala/peer/Hashable.scala
@@ -1,19 +1,53 @@
 package peer
 
 import com.google.common.hash.HashCode
+import com.google.common.hash.Hasher
+import com.google.common.hash.HashFunction
+import com.google.common.hash.Hashing
 
-trait Hashable {
-  def hash(): Seq[Byte]
+trait Hashable[T] {
+
+  def hash(bytes: Seq[Byte]): Code
+
+}
+object Hashable {
+
+  def apply[T: Hashable](): Hashable[T] = implicitly[Hashable[T]]
+
+  implicit val sipHashable = new Hashable[HashCode] {
+
+    val sipHash = Hashing.sipHash24()
+    val hasher = sipHash.newHasher()
+
+    override def hash(bytes: Seq[Byte]): Code =
+      SipHashCode(sipHash, hasher, bytes)
+
+  }
 }
 
-case class Code(value: HashCode) {
+trait Code {
 
-  override def toString(): String = value.toString
+  def toString(): String
 
-  def toBytes(): Array[Byte] = value.asBytes
+  def toBytes(): Array[Byte]
 
-  def toInt(): Int = value.asInt
+  def toInt(): Int
 
-  def toLong(): Long = value.asLong
+  def toLong(): Long
+
+}
+
+case class SipHashCode(sipHash: HashFunction, hasher: Hasher, bytes: Seq[Byte])
+    extends Code {
+
+  protected[peer] val hasCode = hasher.putBytes(bytes.toArray)
+
+  override def toString(): String = hashCode.toString
+
+  def toBytes(): Array[Byte] = bytes.toArray
+
+  def toInt(): Int = hashCode.toInt
+
+  def toLong(): Long = hashCode.toLong
 
 }
--- core/src/main/scala/peer/Id.scala
+++ core/src/main/scala/peer/Id.scala
@@ -1,18 +1,9 @@
 package peer
 
-import com.google.common.hash.Hashing
-
-object Id {
-  def hash(hashable: Hashable): Code = {
-    val hashFn = Hashing.sipHash24()
-    return Code(hashFn.newHasher().putBytes(hashable.hash().toArray).hash)
-  }
-}
 case class Id(bytes: Array[Byte]) {
 
-  override def hashCode(): Int =
-    Id.hash(new Hashable {
-      override def hash(): Seq[Byte] = bytes.toSeq
-    }).toInt()
+  protected[peer] val code = Hashable().hash(bytes.toSeq)
+
+  override def hashCode(): Int = code.toInt()
 
 }
